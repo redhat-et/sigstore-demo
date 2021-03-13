2afc4b0a689b602fa5494252cdc35226861ab42e
Ivan Font <ifont@redhat.com>
2021-03-12 17:50:22 -0800

    Add demo web server app

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello SigStore World!\n")
+}
+
+func main() {
+	// Hello world, the web server
+	http.HandleFunc("/", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
