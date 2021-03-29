c396a7a248cb02da88f5f061d97666444ee374c5
Ivan Font <ifont@redhat.com>
2021-03-29 16:16:51 -0700

    Reset demo app hello world string

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hello Dry Run World on Monday morning!\n")
+	io.WriteString(w, "Hello World!\n")
 }
 
 func main() {
