90cf01b29421af6bf223902f86c79cf6c5225728
Ivan Font <ifont@redhat.com>
2021-03-29 18:07:00 -0700

    Add doitlive sigstore demo script

---
+++ scripts/sigstore-demo.sh
@@ -0,0 +1,69 @@
+#/usr/bin/env bash
+
+# Recorded with the doitlive recorder
+#doitlive shell: /bin/bash
+#doitlive prompt: damoekri
+#doitlive speed: 3
+#doitlive env: COSIGN_EXPERIMENTAL=1
+#doitlive commentecho: true
+
+## DEMO RUNNING APP
+google-chrome https://sigstore-demo-app.apps.sigstore-demo.bcallawa.dev/
+
+# contents of repo
+ls -tr
+
+## Dockerfile for running app
+vi Dockerfile
+
+## UNSIGNED CONTAINER IMAGE
+
+# build base container image
+podman build --no-cache -f config/image/base/Dockerfile . -t gcr.io/ifontlabs/ubi8-minimal:stable
+
+# push base container image
+podman push gcr.io/ifontlabs/ubi8-minimal:stable
+
+# make change to app
+vi main.go
+
+# commit change
+git commit -a -m "Update Hello World message"
+
+# push change
+git push
+
+# pipelinerun
+
+## SIGNED CONTAINER IMAGE
+
+# sign existing container image
+cosign sign gcr.io/ifontlabs/ubi8-minimal:stable
+
+# re-run pipeline and show deployed app changes
+
+# Dockerfile for malicious image
+cat config/image/base/exploit/Dockerfile
+
+# build malicious image
+podman build --no-cache -f config/image/base/exploit/Dockerfile . -t gcr.io/ifontlabs/ubi8-minimal:stable
+
+# push malicious image
+podman push gcr.io/ifontlabs/ubi8-minimal:stable
+
+# sign malicious image
+cosign sign gcr.io/ifontlabs/ubi8-minimal:stable
+
+# make another change to app
+vi main.go
+
+# commit change
+git commit -a -m "Update Hello World message again"
+
+# push change
+git push
+
+# list of maintainers
+vi MAINTAINERS.json
+
+
